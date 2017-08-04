63b7c58dcc713f49ac3dd7d1ce423494ec817620
sang-eun <biglove97@hanmail.net>
2017-08-04 19:51:12 +0900

    test

---
+++ routes/home.js
@@ -0,0 +1,25 @@
+var express  = require('express');
+var router   = express.Router();
+var mongoose = require('mongoose');
+var passport = require('../config/passport.js');
+
+
+router.get('/', function (req,res) {
+  res.render("posts/index.html");
+});
+router.get('/login', function (req,res) {
+  res.render('login/login',{email:req.flash("email")[0], loginError:req.flash('loginError'), loginMessage:req.flash('loginMessage')});
+});
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/posts',
+    failureRedirect : '/login',
+    failureFlash : true
+  })
+);
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.flash("postsMessage", "Good-bye, have a nice day!");
+    res.redirect('/');
+});
+
+module.exports = router;
